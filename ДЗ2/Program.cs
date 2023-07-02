e8293445f1f01ade7fe7ee609f45a65d7a334d6a
Горбунов Вячеслав <sgorbunov957@gmail.com>
2023-07-02 23:46:12 +0300

    омашнее задание 2

---
+++ ДЗ2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+//Console.WriteLine("Введите число!");
+//int a  = int.Parse(Console.ReadLine());
+//while (a >= 1000){
+  //  a=a/10;
+//}
+//int b = a%10;
+//Console.WriteLine($"Третья цифра числа:{b}");
+
+Console.WriteLine("Введите число!");
+int f  = int.Parse(Console.ReadLine());
+string c = Math.Abs(f).ToString();
+if (c.Length < 3){
+    Console.WriteLine("В вашем числе нет третьей цифры");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа: {c[2]}");
+}
